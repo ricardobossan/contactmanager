0cccd6dd6773c724878cbaef571f72b1768d9f13
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-03-06 15:56:12 -0300

    s4c26. Adding A Context Reducer For Actions

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,29 +3,6 @@ import Contact from './Contact';
 import { Consumer } from '../context';
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '222-222-2222'
-      },
-      {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '111-111-1111'
-      }
-    ]
-  };
-
   deleteContact = id => {
     const { contacts } = this.state;
 
@@ -42,11 +19,7 @@ class Contacts extends Component {
           return (
             <React.Fragment>
               {contacts.map(contact => (
-                <Contact
-                  key={contact.id}
-                  contact={contact}
-                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-                />
+                <Contact key={contact.id} contact={contact} />
               ))}
             </React.Fragment>
           );
