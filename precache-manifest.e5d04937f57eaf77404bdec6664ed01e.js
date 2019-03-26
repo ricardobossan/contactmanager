d633c321c14440812c05adbf56416a08a321a24a
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-03-26 18:28:20 -0300

    Updates

---
+++ precache-manifest.e5d04937f57eaf77404bdec6664ed01e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "002d56c5bbbf6d809ae5",
+    "url": "/contactmanager/static/js/main.002d56c5.chunk.js"
+  },
+  {
+    "revision": "769de9eca749b3281ca5",
+    "url": "/contactmanager/static/js/2.769de9ec.chunk.js"
+  },
+  {
+    "revision": "002d56c5bbbf6d809ae5",
+    "url": "/contactmanager/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "769de9eca749b3281ca5",
+    "url": "/contactmanager/static/css/2.481e5fd1.chunk.css"
+  },
+  {
+    "revision": "4f32c960e319a88d1c76a08ee1ca74fe",
+    "url": "/contactmanager/index.html"
+  }
+];
